cf2dc342e4dd8c1f469b4d63a20e1f94a5ff2a2f
Leandro <leandro.a.loiacono@gmail.com>
2020-07-11 21:02:13 +0200

    agrego validacion user login

---
+++ script/login.js
@@ -0,0 +1,65 @@
+'use strict'
+
+class Login {
+    constructor() {
+        this.emailInput = document.querySelector('#email-login')
+        this.passwordInput = document.querySelector('#password-login')
+
+        this.loginButton = document.querySelector('#signin-button')
+        this.messageContainer = document.querySelector('.container-message')
+    }
+
+    //método submit que gestionará envío de los datos (durante event)
+    submit = (event) => {
+        event.preventDefault();
+
+        const usersDB = db.getAllUsers();
+
+        const email = this.emailInput.value;
+        const password = this.passwordInput.value;
+
+        // intentar encontrar el usuario
+        // usamos find porque SOLO devuelve la expresion
+
+        const user = usersDB.find((userObj) => {
+            if (userObj.email === email && userObj.password === password) {
+                return true
+            }
+        })
+
+        this.showMessage(user);
+        // this.redirect()
+
+    }
+
+
+    //showMessage para mostrar un mensaje (error o éxito)
+    showMessage = (user) => {
+        this.messageContainer.innerHTML = ''
+        const message = document.createElement('p')
+
+        if (user) {
+            //si el usuario existe
+            message.innerHTML = `Hola, ${user.email}`;
+            message.classList.add('correct-message');
+        } else {
+            message.innerHTML = 'Email y/o password son incorrectos';
+
+        }
+
+        this.messageContainer.appendChild(message);
+    }
+
+    redirect = () => {
+        setTimeout(() => {
+            location.assign("../index.html")
+        }, 2000)
+
+    }
+
+
+}
+
+const login = new Login();
+
+login.loginButton.addEventListener('click', login.submit);
