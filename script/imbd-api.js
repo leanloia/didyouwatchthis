1723944301c16f2b014eb3f8e4b9d26bcd5b8452
Leandro <leandro.a.loiacono@gmail.com>
2020-07-12 15:11:03 +0200

    no significate change

---
+++ script/imbd-api.js
@@ -0,0 +1,36 @@
+'use strict';
+
+// const getMovie = () => {
+
+//     const carousel = document.querySelector("#carousel-random");
+
+//     for (let i = 0; i < 1; i++) {
+//         fetch('https://imdb-api.com/en/API/Top250Movies/k_Unb8zG5Y')
+//         .then ( (response) => {
+//             return response.json();
+//         })
+//         .then ((data) => {
+//             console.log(data.items[0])
+//         })
+
+//         .catch( (err) => {
+//             console.log('error forro!')
+//         })
+//     }
+
+
+// };
+
+// getMovie();
+
+
+// const movies = async () => {
+//     const response = await 
+//     fetch('https://imdb-api.com/en/API/Top250Movies/k_r4OyjdJv')
+//     const info = await response.json()
+//     const result = info 
+//     console.log(info)
+//   }
+
+//   movies()
+
