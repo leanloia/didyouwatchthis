cfba9bd1bbd0af0c51478817e877e8f14b0b990d
Leandro <leandro.a.loiacono@gmail.com>
2020-07-14 00:56:29 +0200

    separate function searchMovie to another .js

--- script/imbd-api.js
+++ script/imbd-api.js
@@ -2,7 +2,7 @@
 
 const getRandomMovies = async () => {
     // fetch a end-point de Top250Movies
-    // APIkey = k_r4OyjdJv
+    // APIkey = k_r4OyjdJv !!!
     
 
     const response = await fetch('https://imdb-api.com/en/API/Top250Movies/k_r4OyjdJv')
@@ -44,54 +44,7 @@ const getRandomMovies = async () => {
     imageCont.appendChild(imgCont)
     
 };
+
 window.addEventListener('load', getRandomMovies)
 
 
-const searchMovie = async () => {
-    
-    redirect = () => {
-        location.assign("/moviedetails.html")
-    }
-    
-    const searchBar = document.querySelector(".search-bar")
-    const searchInput = searchBar.value
-    
-    const response = await fetch(`https://imdb-api.com/en/API/SearchMovie/k_r4OyjdJv/${searchInput}`)
-    const resultSearch = await response.json()
-    const resultId = resultSearch.results[0].id
-    
-    const idSearch = await fetch(`https://imdb-api.com/en/API/Title/k_r4OyjdJv/${resultId}`)
-    const movieResult = await idSearch.json()
-    
-    
-    const movieFullTitle = movieResult.fullTitle
-    const movieReleaseDate = movieResult.releaseDate
-    const movieDuration = movieResult.runtimeStr
-    const moviePlot = movieResult.plot
-    const movieAwards = movieResult.awards
-    const movieDirector = movieResult.directors
-    const movieStars = movieResult.stars
-    const movieWriters = movieResult.writers
-    const movieGenre = movieResult.genres
-    const movieImDbRate = movieResult.imDbRating
-    const movieBoxOffice = movieResult.boxOffice.cumulativeWorldwideGross
-    const movieImg = movieResult.image
-    
-    const imgDetailCont = document.querySelector('.container-details')
-    const imgContainer = document.createElement(`img`)
-    const titleCont = document.createElement(`p`)
-    imgContainer.src = `${movieImg}`
-    imgContainer.alt = `${movieFullTitle}`
-    imgContainer.classList.add('img-cont')
-    titleCont.innerHTML = `${movieFullTitle}`
-    titleCont.classList.add('titleMovieSearch')
-    imgDetailCont.innerHTML = ''
-    imgDetailCont.appendChild(titleCont);
-    imgDetailCont.appendChild(imgContainer);
-    
-    redirect();
-
-}
-
-const searchBTN = document.querySelector('.search-btn')
-searchBTN.addEventListener('click', searchMovie)
