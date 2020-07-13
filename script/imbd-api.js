3c69521b818cd3c09f2fee25de800fc032de65b6
Leandro <leandro.a.loiacono@gmail.com>
2020-07-13 16:56:59 +0200

    add comments to clarify 2nd fetch end-point

--- script/imbd-api.js
+++ script/imbd-api.js
@@ -1,26 +1,23 @@
 'use strict';
 
 const getRandomMovies = async () => {
-
+    // fetch a end-point de Top250Movies
     const response = await fetch('https://imdb-api.com/en/API/Top250Movies/k_r4OyjdJv')
     const info = await response.json()
     const arrMovies = info.items
-
+    // random sobre nº de elementos en el array obtenido
     let randomNum = Math.floor((Math.random()) * (arrMovies.length))
     let randomMovie = arrMovies[randomNum];
 
-
-
-
-    console.log(randomMovie)
-
+    
     const movieId = randomMovie.id
     const movieTitle = randomMovie.title
     const movieYear = randomMovie.year
     const movieImDbRating = randomMovie.imDbRating
     const movieCrew = randomMovie.crew
-    // const movieImage = randomMovie.image
+    
 
+    //fetch a 2do end-point de Posters (por mejor calidad de imágenes)
     const resp = await fetch(`https://imdb-api.com/en/API/Posters/k_r4OyjdJv/${movieId}`)
     const postersObj = await resp.json()
     const movieImage = postersObj.posters[0].link
