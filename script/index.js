fdcec3a80e1e497a7fb6a661dc0ea023c6638fca
Leandro <leandro.a.loiacono@gmail.com>
2020-07-14 16:30:49 +0200

    modify mySidenav width for mobile first

--- script/index.js
+++ script/index.js
@@ -1,7 +1,7 @@
 /* Set the width of the side navigation to 250px and the left margin of the page content to 250px and add a black background color to body */
 function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
-    document.getElementById("main").style.marginRight = "250px";
+    document.getElementById("mySidenav").style.width = "50%";
+    document.getElementById("main").style.marginRight = "50%";
     document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
   }
   
