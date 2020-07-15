ef94adda52e766d2050bc44a447842212b2739c1
Leandro <leandro.a.loiacono@gmail.com>
2020-07-15 18:37:10 +0200

    add conditional to closeNav for mediaqueries

--- script/index.js
+++ script/index.js
@@ -1,14 +1,20 @@
 /* Set the width of the side navigation to 250px and the left margin of the page content to 250px and add a black background color to body */
+
+
 function openNav() {
-    document.getElementById("mySidenav").style.width = "50%";
-    document.getElementById("main").style.marginRight = "50%";
-    document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+  document.getElementById("mySidenav").style.width = "40%";
+  document.getElementById("main").style.marginRight = "40%";
+  document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+}
+
+/* Set the width of the side navigation to 0 and the left margin of the page content to 0, and the background color of body to white */
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+  document.getElementById("main").style.marginRight = "0";
+  document.body.style.backgroundColor = "white";
+
+  if (window.innerWidth >= 768 px) {
+    return false;
   }
-  
-  /* Set the width of the side navigation to 0 and the left margin of the page content to 0, and the background color of body to white */
-  function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
-    document.getElementById("main").style.marginRight = "0";
-    document.body.style.backgroundColor = "white";
-    
-  }
+
+}
